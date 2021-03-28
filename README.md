dbfe9a8c84de9d697e85dc57b6de465c791e1cf7
Parwinder <psroadheaven@gmail.com>
2021-03-29 02:11:43 +0530

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# hello-github
-
-
-
-
-
-i did a lot of bug change here and implemented new bugs 
--- src/components/header.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Header = ({ heading }) => {
-  return <h1 className='heading'>{heading} </h1>
-};
-
-export default Header;
